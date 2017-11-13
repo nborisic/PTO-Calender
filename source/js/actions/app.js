4d29c6ce04d60e544287df62805f342e3c684787
Nikola Borisic <borisic.nikola@yahoo.com>
2017-11-13 18:08:01 +0100

    initial commit

--- source/js/actions/app.js
+++ source/js/actions/app.js
@@ -1,18 +1,7 @@
-import api from 'api';
-
-export const TEST_ACTION = 'TEST_ACTION';
-
 export const TEST_ASYNC_ACTION_START = 'TEST_ASYNC_ACTION_START';
 export const TEST_ASYNC_ACTION_ERROR = 'TEST_ASYNC_ACTION_ERROR';
 export const TEST_ASYNC_ACTION_SUCCESS = 'TEST_ASYNC_ACTION_SUCCESS';
 
-// Test action
-
-export function testAction() {
-  return {
-    type: TEST_ACTION,
-  };
-}
 
 // Async action example
 
@@ -40,10 +29,8 @@ export function testAsync() {
   return function (dispatch) {
     dispatch(testAsyncStart());
 
-    api.testAsync()
-      .then(data => dispatch(testAsyncSuccess(data)))
-      .catch(error => dispatch(testAsyncError(error)));
+    // api.testAsync()
+    //   .then(data => dispatch(testAsyncSuccess(data)))
+    //   .catch(error => dispatch(testAsyncError(error)));
   };
 }
-
-// Update
--- source/js/views/App/index.jsx
+++ source/js/views/App/index.jsx
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
 import Routes from 'config/routes';
 
-import Menu from 'components/Global/Menu';
 
 export default class App extends Component {
   render() {
     return (
       <div className='App'>
-        <Menu />
 
         <div className='Page'>
           <Routes />
