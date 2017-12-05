7ef6460f22761150243b26dfdf19d8f408467e7e
Nikola Borisic <borisic.nikola@yahoo.com>
2017-12-05 10:38:51 +0100

    replace svg for inline svg

--- source/js/components/Global/FilterGroup.jsx
+++ source/js/components/Global/FilterGroup.jsx
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { sortDropdownArray } from 'utils/global';
 import { addFilter, removeFilter } from 'actions/filter';
-import filterSvg from '../../../assets/img/filterButton.svg';
 
 @connect(state => ({
   filter: state.filter.get('filter'),
@@ -400,7 +399,13 @@ export default class FilterGroup extends Component {
               className={ buttonClass }
               onClick={ this.handleFilterClick }
             >
-              <img src={ filterSvg } alt='filterButton' />
+              <svg width='30px' height='30px' viewBox='0 0 27 27' version='1.1' xmlns='http://www.w3.org/2000/svg' xmlnsXlink='http://www.w3.org/1999/xlink'>
+                <circle id='Oval' fill='#272727' cx='13.5' cy='13.5' r='13.5' />
+                <g id='Group-3' transform='translate(7.000000, 7.000000)' stroke='#FFFFFF' strokeWidth='2' strokeLinecap='round'>
+                  <path d='M2.11472404,2.61799201 L10.1670116,10.6702796' id='Line' transform='translate(6.644136, 6.644136) rotate(-45.000000) translate(-6.644136, -6.644136) ' />
+                  <path d='M2.47058824,10.5944081 L10.5587211,2.50627531' id='Line' transform='translate(7.000000, 7.000000) rotate(-45.000000) translate(-7.000000, -7.000000) ' />
+                </g>
+              </svg>
             </button>
             <div className='filterDrop'>
               <div>
--- source/js/components/Global/ScrollButtons.jsx
+++ source/js/components/Global/ScrollButtons.jsx
@@ -12,11 +12,61 @@ export default class ScrollButtons extends Component {
         <button
           className='leftButton'
           onClick={ () => { this.props.scrollClick(-1); } }
-        />
+        >
+          <svg width='30px' height='30px' viewBox='0 0 27 27' version='1.1' xmlns='http://www.w3.org/2000/svg' xmlnsXlink='http://www.w3.org/1999/xlink'>
+            <g id='Horizontally-Scrolling' transform='translate(-1343.000000, -66.000000)'>
+              <g id='Group' transform='translate(1307.000000, 66.000000)'>
+                <g id='Group-9' transform='translate(36.720000, 0.000000)'>
+                  <rect
+                    id='Rectangle-6'
+                    fill='#272727'
+                    transform='translate(12.960000, 13.500000) rotate(-180.000000) translate(-12.960000, -13.500000) '
+                    x='0'
+                    y='0'
+                    width='25.92'
+                    height='27'
+                    rx='12.96'
+                  />
+                  <polyline
+                    id='Page-1'
+                    fill='#FFFFFF'
+                    transform='translate(13.983158, 13.144737) rotate(-360.000000) translate(-13.983158, -13.144737) '
+                    points='15.9116603 12.0703308 12.0891297 8.52631579 10.9470515 9.58415414 14.770689 13.1293421 10.9136842 16.7053195 12.0546555 17.7631579 17.0526316 13.1293421'
+                  />
+                </g>
+              </g>
+            </g>
+          </svg>
+        </button>
         <button
           className='rightButton'
           onClick={ () => { this.props.scrollClick(1); } }
-        />
+        >
+          <svg width='30px' height='30px' viewBox='0 0 27 27' version='1.1' xmlns='http://www.w3.org/2000/svg' xmlnsXlink='http://www.w3.org/1999/xlink'>
+            <g id='Horizontally-Scrolling' transform='translate(-1343.000000, -66.000000)'>
+              <g id='Group' transform='translate(1307.000000, 66.000000)'>
+                <g id='Group-9' transform='translate(36.720000, 0.000000)'>
+                  <rect
+                    id='Rectangle-6'
+                    fill='#272727'
+                    transform='translate(12.960000, 13.500000) rotate(-180.000000) translate(-12.960000, -13.500000) '
+                    x='0'
+                    y='0'
+                    width='25.92'
+                    height='27'
+                    rx='12.96'
+                  />
+                  <polyline
+                    id='Page-1'
+                    fill='#FFFFFF'
+                    transform='translate(13.983158, 13.144737) rotate(-360.000000) translate(-13.983158, -13.144737) '
+                    points='15.9116603 12.0703308 12.0891297 8.52631579 10.9470515 9.58415414 14.770689 13.1293421 10.9136842 16.7053195 12.0546555 17.7631579 17.0526316 13.1293421'
+                  />
+                </g>
+              </g>
+            </g>
+          </svg>
+        </button>
       </div>
     );
   }
